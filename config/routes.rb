9be3c6e3290dd9af01d946cef8d86e2d2d595b22
allyssoncomin <allysson_luan@hotmail.com>
2024-07-31 16:23:46 -0300

    ajust routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # devise_for :users
   root to: "pages#home"
+  get 'home', to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'projects', to: 'pages#projects'
+  get 'contact', to: 'pages#contact'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
